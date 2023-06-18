09a91df6e9274fe5f0081557892e775ad9077d15
mobin <pnamobin@gmail.com>
2023-06-18 17:28:20 +0330

    Add basics for function 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
+// int diffCalculator(int a, int b)
+// {
+
+//     if (b-a >= 7)
+//     {
+//         /* code */
+//     }
+    
+
+// }
+
+void newEntry()
+{
+       
+    long long int codemelli;
+    string name;
+    int clockinhour, clockinmin;
+    int clockouthour, clockoutmin;
+    int overtimehour, overtimemin;
+    int undertimehour, undertimemin;
+    
+    fstream file;
+    file.open("File.txt", ios::app);
+    if (file)
+    {
+        
+        cout << "Please enter your id code.\n";
+        cin >> codemelli; 
+        cout << "Please enter your full name.\n";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Please enter your clock-in hour and miunte accordingly.\n";
+        cin >> clockinhour >> clockinmin ;
+        cout << "Please enter your clock-out hour and miunte accordingly.\n";
+        cin >> clockouthour >> clockoutmin;
+        // diffCalculator(clockinhour, clockouthour);
+        file << codemelli << "#" << name << "#" 
+        << clockinhour << ":" << clockinmin << "#"
+        <<clockouthour << ":" << clockoutmin << "#" << endl;
+
+    }
+    else
+        cerr << "There is a problem with your file.\n";
+    system("cls");
+
+}
 
 int main()
 {   
@@ -16,7 +63,7 @@ int main()
         switch (option)
         {
         case 1:
-            cout << "New entry\n";
+            newEntry();
             break;
         case 2:
             cout << "Edit entry\n";
