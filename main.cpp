1fe975bc03294c9b4dd68effeb730a8e0d168552
mobin <pnamobin@gmail.com>
2023-06-18 11:59:11 +0330

    Create the structure and menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    cout << "Hello World!" ;
+
+int main()
+{   
+    int option;
+    cout << "Welcome\n";
+    do
+    {
+        cout << "1. New entry\n2. Edit past entry\n0. Exit\n";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            cout << "New entry\n";
+            break;
+        case 2:
+            cout << "Edit entry\n";
+            break;
+        case 0:
+            cout << "Goodbye\n";
+            exit;
+        default:
+            cout << "Option not valid\nPlease try again\n";
+            break;
+        }
+    } while (option != 0);
+    
     
 }
